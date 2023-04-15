7e453dea870f55e3cffa1df1307dd9b211786e6d
Axel Velasco Chávez <chavezsud1406@gmail.com>
2023-04-15 01:50:40 -0600

    Se implementa la funcionalidad para mostrar la ventana para crear una tarea

---
+++ app.js
@@ -0,0 +1,21 @@
+/* Varibles */
+const nuevaTareaBtn = document.querySelector('#nuevaTareaBtn')
+const pantallaBlanca_NewTarea = document.querySelector('#pantallaBlanca--newTarea')
+
+/* Funciones */
+const interfazNewTarea = () =>{
+    pantallaBlanca_NewTarea.classList.toggle('pantallaBlanca--off')
+    rotarBtnNewTarea()
+}
+const esconderNewTarea = e =>{
+    if(e.target.classList.contains('pantallaBlanca')){
+        pantallaBlanca_NewTarea.classList.add('pantallaBlanca--off')
+    }
+}
+
+const rotarBtnNewTarea = () =>{
+    nuevaTareaBtn.classList.toggle('nuevaTarea--active')
+}
+/* Eventos */
+nuevaTareaBtn.addEventListener('click', interfazNewTarea)
+pantallaBlanca_NewTarea.addEventListener('click', esconderNewTarea)
